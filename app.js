4b53e451d8803001b78d8f92c349bad1c2fc8948
kevin park <kevin.park02@gmail.com>
2020-12-21 09:30:05 -0800

    Delete the mongoose uri from app

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-// mongodb+srv://kevinpark02:lUsZ1TNUbzDvGaq7@cluster0.tcw4p.mongodb.net/<dbname>?retryWrites=true&w=majority
-
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
